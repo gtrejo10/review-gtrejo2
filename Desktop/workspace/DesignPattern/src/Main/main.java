e84ccd31b3ba9b2406e2a41d1e027b1f3e613799
Gema Trejo <gtrejo2@asu.edu>
2019-04-15 04:30:04 -0700

    Initial commit

---
+++ Desktop/workspace/DesignPattern/src/Main/main.java
@@ -0,0 +1,15 @@
+/**
+ * Main class for all of my design patterns to run in
+ */
+package Main;
+
+/**
+ * @author gtrejo2
+ *
+ */
+public class main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+}
